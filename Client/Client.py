3066915b483c36acf5f88815dfa6d13b35716199
Ola Aunrønning <ola.aunroe@gmail.com>
2015-02-27 13:36:56 +0100

    Finished init process. Begun work on the chat loop

--- Client/Client.py
+++ Client/Client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
+import MessageReceiver
 
 class Client:
     """
@@ -13,24 +14,39 @@ class Client:
 
         # Set up the socket connection to the server
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host=host
+        self.server_port=server_port
         self.run()
-
         # TODO: Finish init process with necessary code
 
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
+        message_receiver = MessageReceiver(self, self.connection)
+        print "Welcome! Type in your input"
+        while True:
+            data=input('')
+            if data=='logout':
+                self.disconnect()
+                print('Disconnected')
+                break
+            self.send_payload(data)
+            print(self.recieve_message)
+
 
     def disconnect(self):
-        # TODO: Handle disconnection
+        self.connection.close()
+        #Do more here probably
         pass
 
     def receive_message(self, message):
         # TODO: Handle incoming message
+        #Use the MessageReceiver handle this
         pass
 
     def send_payload(self, data):
         # TODO: Handle sending of a payload
+        self.connection.sendto(data,(self.host,self.server_port))
         pass
 
 
