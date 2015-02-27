f8e5e2c411e399c6015edff6086633aeac31869f
Ivar Hessevik <ivarhh@stud.ntnu.no>
2015-02-27 13:47:48 +0100

    MessageReciever should now work.

--- Client/MessageReceiver.py
+++ Client/MessageReceiver.py
@@ -15,9 +15,32 @@ class MessageReceiver(Thread):
 
         # Flag to run thread as a deamon
         self.daemon = True
-
-        # TODO: Finish initialization of MessageReceiver
+		self.connection = connection
+		self.client = client
+		self.run()
 
     def run(self):
-        # TODO: Make MessageReceiver receive and handle payloads
-        pass
+        while True:
+			received_string = self.connection.recv(4096)
+			
+			payload = parse_message(received_string)
+			timestamp = payload["timestamp"]
+			sender = payload["sender"]
+			response = payload["response"]
+			content = payload["content"]
+			
+			message = "{1} - {0} [{2}]:\n".format(timestamp,sender,response)
+			
+			#Make indentation. Split because when respnse = history.
+			message = "\t" + "\t\n".join(content.split("\n"))
+			
+			print message
+			
+	def parse_message(message):
+		"""
+		Parses message from string format to dictionary. If the payload is an invalid json-format, return None.
+		"""
+		try:
+			return json.loads(message)
+		except ValueError:
+			return None
